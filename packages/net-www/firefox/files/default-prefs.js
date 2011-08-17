8b68d6147868278ff6c21978bba625d1d04bae3d
Quentin Glidic <sardemff7.pub@gmail.com>
2011-08-17 03:01:16 +0200

    Add Mozilla SCM stuff

---
+++ packages/net-www/firefox/files/default-prefs.js
@@ -0,0 +1,5 @@
+pref("browser.tabs.tabMinWidth",           15);
+pref("browser.backspace_action",           0);
+
+# Let the system provide the choice UI
+pref("browser.shell.checkDefaultBrowser",  false);
