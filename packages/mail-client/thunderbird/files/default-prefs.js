48b4fc9f783ae9e58c2fe234e508237002f4f478
Quentin Glidic <sardemff7+git@sardemff7.net>
2011-10-08 18:22:18 +0200

    Will re-add some of these later

--- packages/mail-client/thunderbird/files/default-prefs.js
+++
@@ -1,9 +0,0 @@
-# Do not switch to Smart Folders after upgrade to 3.0b4
-pref("mail.folder.views.version", "1");
-
-# Upper is newer, as "expected"
-pref("mailnews.default_news_sort_order", "2");
-pref("mailnews.default_sort_order", "2");
-
-# Let the system provide the choice UI
-pref("mail.shell.checkDefaultClient", false);
